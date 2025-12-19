0ee7401574326cb651d595f53da2d089326219bf
Exionot <exion242007@gmail.com>
2025-12-19 11:17:46 +0800

    Added home page

--- Arcane Binder/src/application/HomeController.java
+++ Arcane Binder/src/application/HomeController.java
@@ -1,41 +1,20 @@
 package application;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HomeController {
-	@FXML private ScrollPane homePanel;
-	@FXML private Button homeButton, charactersButton, galleryButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class HomeController implements Initializable{
+	@FXML private Label welcomeLabel;
 	
-	public void changePanel(ActionEvent e) {
-		if (((Button)e.getSource()).getId() == homeButton.getId()) {
-//			loadPanel("Home");
-		}
-		else if (((Button)e.getSource()).getId() == charactersButton.getId()) {
-			loadPanel("Characters", CharactersController.class, controller -> {
-				controller.refresh(homePanel);
-			});
-		}
-		else if (((Button)e.getSource()).getId() == galleryButton.getId()) {
-			loadPanel("Gallery", GalleryController.class, controller -> {
-				controller.refresh(homePanel);
-			});
-		}
+	private String username = Main.config.getName();
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		welcomeLabel.setText("Welcome Back, " + username);
 	}
 	
-	private <Controller> void loadPanel(String id, Class<Controller> controller, java.util.function.Consumer<Controller> controllerAction) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(application.Main.FXML_DIR + id + ".fxml"));
-			ScrollPane charPanel = loader.load();
-			Controller panelController = loader.getController();
-			controllerAction.accept(panelController);
-			charPanel.setPickOnBounds(true);
-			homePanel.setContent(charPanel);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
